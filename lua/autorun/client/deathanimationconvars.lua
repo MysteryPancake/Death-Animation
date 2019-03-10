c944e9818e1645a05fd3fda2ce681a09a5e32817
MysteryPancake <MysteryPancake@users.noreply.github.com>
2019-03-10 13:19:18 +1100

    Revert "Renamed some files and changed some things (TESTING NEEDED)"
    This reverts commit cddee1d09345a7dc6666d8cbbad479dbde82a8c4.

--- lua/autorun/client/deathanimationconvars.lua
+++ lua/autorun/client/deathanimationconvars.lua
@@ -1,4 +1,7 @@
 
+-- This file contains all the client convars to do with the animations, because each client should be able to choose their death animation, right?
+-- I don't think this makes the script exploitable though, if any errors happen, the one setting these convars is usually the one that suffers
+
 CreateClientConVar( 'deathanimation_random', 'death_01,death_02,death_03', true, true, 'Changes the table of random animations to play.' )
 
 CreateClientConVar( 'deathanimation_headshot', 'death_04', true, true, 'Changes the head-shot death animation.' )
@@ -9,14 +12,3 @@ CreateClientConVar( 'deathanimation_rightarm', '%random_anim%', true, true, 'Cha
 CreateClientConVar( 'deathanimation_leftleg', '%random_anim%', true, true, 'Changes the left-leg death animation.' )
 CreateClientConVar( 'deathanimation_rightleg', '%random_anim%', true, true, 'Changes the right-leg death animation.' )
 CreateClientConVar( 'deathanimation_generic', '%random_anim%', true, true, 'Changes the generic death animation.' )
-
-net.Receive( 'DeathAnimation_RagColor', function()
-
-	local rag = net.ReadEntity()
-	local color = net.ReadNormal()
-	
-	if !IsValid( rag ) then return end
-	
-	rag.GetPlayerColor = function() return color end
-
-end )
