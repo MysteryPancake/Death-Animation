66955d6ac48b2e9c3f1b4bf615b86c436caf7dac
MysteryPancake <MysteryPancake@users.noreply.github.com>
2019-03-10 13:17:36 +1100

    Revert "Changed some more stuff (STILL TESTING)"
    This reverts commit fa313b82a376d29f7e44e0469e65cbb484d4d09b.

--- lua/autorun/client/cl_deathanimation.lua
+++ lua/autorun/client/cl_deathanimation.lua
@@ -1,18 +1,6 @@
 
--- Random animations
 CreateClientConVar( 'deathanimation_random', 'death_01,death_02,death_03', true, true, 'Changes the table of random animations to play.' )
 
--- First person
-CreateClientConVar( 'deathanimation_firstperson', '0', true, true, 'Toggles first person death view' )
-
--- Basic stuff
-CreateClientConVar( 'deathanimation_kill', '%no_anim_norag%', true, true, 'Changes the kill-command death animation.' )
--- CreateClientConVar( 'deathanimation_offground', '%no_anim_norag%', true, true, 'Changes the on-ground death animation.' ) -- Too general
-CreateClientConVar( 'deathanimation_fire', 'death_02', true, true, 'Changes the burning death animation.' )
-CreateClientConVar( 'deathanimation_drown', '%no_anim_ragdoll%', true, true, 'Changes the drowning death animation.' )
-CreateClientConVar( 'deathanimation_explosion', '%no_anim_ragdoll%', true, true, 'Changes the explosion death animation.' )
-
--- Hitgroups
 CreateClientConVar( 'deathanimation_headshot', 'death_04', true, true, 'Changes the head-shot death animation.' )
 CreateClientConVar( 'deathanimation_chestshot', '%random_anim%', true, true, 'Changes the chest-shot death animation.' )
 CreateClientConVar( 'deathanimation_stomachshot', '%random_anim%', true, true, 'Changes the stomach-shot death animation.' )
@@ -20,48 +8,15 @@ CreateClientConVar( 'deathanimation_leftarm', '%random_anim%', true, true, 'Chan
 CreateClientConVar( 'deathanimation_rightarm', '%random_anim%', true, true, 'Changes the right-arm death animation.' )
 CreateClientConVar( 'deathanimation_leftleg', '%random_anim%', true, true, 'Changes the left-leg death animation.' )
 CreateClientConVar( 'deathanimation_rightleg', '%random_anim%', true, true, 'Changes the right-leg death animation.' )
-
--- Generic
 CreateClientConVar( 'deathanimation_generic', '%random_anim%', true, true, 'Changes the generic death animation.' )
 
-local enabled = GetConVar( 'deathanimation_enabled' )
+net.Receive( 'DeathAnimation_RagColor', function()
 
-local prevply, prevrag
-
-net.Receive( 'DeathAnimationClient', function()
-
-	local ply = net.ReadEntity()
-	local ragind = net.ReadInt( 32 ) -- The ragdoll doesn't exist on the client yet
+	local rag = net.ReadEntity()
+	local color = net.ReadNormal()
 	
-	if !IsValid( ply ) or !ply:IsPlayer() or !isnumber( ragind ) then return end
-	prevply, prevrag = ply, ragind
+	if !IsValid( rag ) then return end
 	
-end )
+	rag.GetPlayerColor = function() return color end
 
-hook.Add( 'NetworkEntityCreated', 'DeathAnimationColorRagdoll', function( ent )
-
-	if IsValid( ent ) and ent:IsRagdoll() and IsValid( prevply ) and prevply:IsPlayer() and isnumber( prevrag ) then
-	
-		local rag = Entity( prevrag )
-		
-		if ( ent == rag ) then
-			ent.GetPlayerColor = function() return Vector( prevply:GetInfo( "cl_playercolor" ) ) end
-			prevply.Ragdoll = ent
-		end
-		
-	end
-	
-end )
-
-local firstperson = GetConVar( 'deathanimation_firstperson' )
-
-hook.Add( 'CalcView', 'DeathAnimationFirstPerson', function( ply )
-
-	if enabled:GetBool() and !ply:Alive() and firstperson:GetBool() and IsValid( ply.Ragdoll ) then
-	
-		local campos = ply.Ragdoll:GetAttachment( ply.Ragdoll:LookupAttachment( "eyes" ) )
-		return { origin = campos.Pos, angles = campos.Ang }
-		
-	end
-	
 end )
--- lua/autorun/server/sv_deathanimation.lua
+++ lua/autorun/server/sv_deathanimation.lua
@@ -2,17 +2,12 @@
 AddCSLuaFile( 'autorun/client/deathanimationmenu.lua' )
 AddCSLuaFile( 'autorun/client/cl_deathanimation.lua' )
 
-util.AddNetworkString( 'DeathAnimationClient' )
+local enabled = CreateConVar( 'deathanimation_enabled', '1', { FCVAR_REPLICATED, FCVAR_NOTIFY }, 'Sets whether the death animations are enabled.' )
+local onground = CreateConVar( 'deathanimation_onground', '0', { FCVAR_REPLICATED, FCVAR_NOTIFY }, 'Sets whether the death animations should only play when a player dies while on the ground.' )
 
-CreateConVar( 'deathanimation_enabled', '1', { FCVAR_REPLICATED, FCVAR_NOTIFY }, 'Sets whether the death animations are enabled.' )
-CreateConVar( 'deathanimation_keepragdolls', '0', { FCVAR_REPLICATED, FCVAR_NOTIFY }, 'Sets whether death ragdolls should be kept after death rather than being removed.' )
+util.AddNetworkString( 'DeathAnimation_RagColor' )
 
-local enabled = GetConVar( 'deathanimation_enabled' )
-local keeprags = GetConVar( 'deathanimation_keepragdolls' )
-
-local LastDamageData = {} -- Used to store damage data to use later
-
-local function CheckAnimation( cvar, ply )
+local function CheckForRandomAnim( cvar, ply ) -- Used to check if a convar is '%random_anim%', and also to verify a bit of other stuff
 
 	if !isstring( cvar ) or cvar == '' then return 'death_0'..math.random( 1, 4 ) end
 	
@@ -30,46 +25,25 @@ local function CheckAnimation( cvar, ply )
 	
 end
 
-local function GetAppropriateAnimation( ply, hitgroup, dmginfo )
-
-	-- Basic stuff
-	if ( dmginfo:GetInflictor() == ply ) then return ply:GetInfo( 'deathanimation_kill' ) end
-	-- if !ply:OnGround() then return ply:GetInfo( 'deathanimation_offground' ) end -- Too general
-	if dmginfo:IsDamageType( DMG_BURN ) then return ply:GetInfo( 'deathanimation_fire' ) end
-	if dmginfo:IsDamageType( DMG_DROWN ) then return ply:GetInfo( 'deathanimation_drown' ) end
-	if dmginfo:IsExplosionDamage() then return ply:GetInfo( 'deathanimation_explosion' ) end
-	
-	-- Hitgroups
-	if ( hitgroup == HITGROUP_HEAD ) then return ply:GetInfo( 'deathanimation_headshot' ) end
-	if ( hitgroup == HITGROUP_CHEST ) then return ply:GetInfo( 'deathanimation_chestshot' ) end
-	if ( hitgroup == HITGROUP_STOMACH ) then return ply:GetInfo( 'deathanimation_stomachshot' ) end
-	if ( hitgroup == HITGROUP_LEFTARM ) then return ply:GetInfo( 'deathanimation_leftarm' ) end
-	if ( hitgroup == HITGROUP_RIGHTARM ) then return ply:GetInfo( 'deathanimation_rightarm' ) end
-	if ( hitgroup == HITGROUP_LEFTLEG ) then return ply:GetInfo( 'deathanimation_leftleg' ) end
-	if ( hitgroup == HITGROUP_RIGHTLEG ) then return ply:GetInfo( 'deathanimation_rightleg' ) end
-	
-	-- Generic
-	return ply:GetInfo( 'deathanimation_generic' )
-	
-end
-
-local function GetProperPlayerModel( ply )
-
-	if ply.getPreferredModel then return ply:getPreferredModel( ply:Team() ) end -- DarkRP
-	
-	local cl_playermodel = ply:GetInfo( "cl_playermodel" )
-	local modelpath = player_manager.TranslatePlayerModel( cl_playermodel )
-	
-	if util.IsValidRagdoll( modelpath ) then
-		return modelpath
-	else
-		return ply:GetModel()
+local function SetEntityStuff( ent1, ent2 ) -- Transfer most of the set things on entity 2 to entity 1
+	if !IsValid( ent1 ) or !IsValid( ent2 ) then return false end
+	ent1:SetModel( ent2:GetModel() )
+	ent1:SetPos( ent2:GetPos() )
+	ent1:SetAngles( ent2:GetAngles() )
+	ent1:SetColor( ent2:GetColor() )
+	ent1:SetSkin( ent2:GetSkin() )
+	ent1:SetFlexScale( ent2:GetFlexScale() )
+	for i = 0, ent2:GetNumBodyGroups() - 1 do ent1:SetBodygroup( i, ent2:GetBodygroup( i ) ) end
+	for i = 0, ent2:GetFlexNum() - 1 do ent1:SetFlexWeight( i, ent2:GetFlexWeight( i ) ) end
+	for i = 0, ent2:GetBoneCount() do
+		ent1:ManipulateBoneScale( i, ent2:GetManipulateBoneScale( i ) )
+		ent1:ManipulateBoneAngles( i, ent2:GetManipulateBoneAngles( i ) )
+		ent1:ManipulateBonePosition( i, ent2:GetManipulateBonePosition( i ) )
+		ent1:ManipulateBoneJiggle( i, ent2:GetManipulateBoneJiggle( i ) )
 	end
-
 end
 
 local function AllowBoneMovement( ragdoll, bool ) -- Changes whether ragdolls can move
-	if !IsValid( ragdoll ) then return end
 	for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
 	local bone = ragdoll:GetPhysicsObjectNum( i )
 		if ( IsValid( bone ) ) then
@@ -81,127 +55,78 @@ end
 local function TransferBones( base, ragdoll ) -- Transfers the bones of one entity to a ragdoll's physics bones (modified version of some of RobotBoy655's code)
 	if !IsValid( base ) or !IsValid( ragdoll ) then return end
 	for i = 0, ragdoll:GetPhysicsObjectCount() - 1 do
-		local physbone = ragdoll:GetPhysicsObjectNum( i )
-		if ( IsValid( physbone ) ) then
+		local bone = ragdoll:GetPhysicsObjectNum( i )
+		if ( IsValid( bone ) ) then
 			local pos, ang = base:GetBonePosition( ragdoll:TranslatePhysBoneToBone( i ) )
-			if ( pos ) then physbone:SetPos( pos, true ) end
-			if ( ang ) then physbone:SetAngles( ang, true ) end
+			if ( pos ) then bone:SetPos( pos ) end
+			if ( ang ) then bone:SetAngles( ang ) end
 		end
 	end
 end
 
-local function SetEntityStuff( ent, ply ) -- Transfer most of the set things on the player to ent
-
-	if !IsValid( ent ) or !IsValid( ply ) then return false end
-	
-	ent:SetModel( GetProperPlayerModel( ply ) )
-	ent:SetPos( ply:GetPos() )
-	ent:SetAngles( ply:GetAngles() )
-	ent:SetColor( ply:GetColor() )
-	ent:SetSkin( ply:GetInfoNum( "cl_playerskin", 0 ) )
-	ent:SetFlexScale( ply:GetFlexScale() )
-	ent:SetOwner( ply )
-	if CPPI then
-		ent:CPPISetOwner( ply )
-	end
-	
-	local groups = string.Explode( " ", ply:GetInfo( "cl_playerbodygroups" ) or "" )
-	for i = 0, ply:GetNumBodyGroups() - 1 do
-		ent:SetBodygroup( i, tonumber( groups[ i + 1 ] ) or ply:GetBodygroup( i ) )
-	end
-	
-	local flexes = {}
-	local flexcvar = GetConVar( "sv_playermodel_selector_flexes" ) -- Enhanced playermodel selector
-	if flexcvar and flexcvar:GetBool() and tobool( ply:GetInfoNum( "cl_playermodel_selector_unlockflexes", 0 ) ) then
-		flexes = ( string.Explode( " ", ply:GetInfo( "cl_playerflexes" ) ) or "" )
-	end
-	for i = 0, ply:GetFlexNum() - 1 do
-		ent:SetFlexWeight( i, tonumber( flexes[ i + 1 ] ) or ply:GetFlexWeight( i ) )
-	end
-	
-	for i = 0, ply:GetBoneCount() do
-		ent:ManipulateBoneScale( i, ply:GetManipulateBoneScale( i ) )
-		ent:ManipulateBoneAngles( i, ply:GetManipulateBoneAngles( i ) )
-		ent:ManipulateBonePosition( i, ply:GetManipulateBonePosition( i ) )
-		ent:ManipulateBoneJiggle( i, ply:GetManipulateBoneJiggle( i ) )
-	end
-	
-	ent.EntityMods = ply.EntityMods
-	ent.BoneMods = ply.BoneMods
-	
-	duplicator.ApplyEntityModifiers( nil, ent )
-	duplicator.ApplyBoneModifiers( nil, ent )
-	
-end
-
-hook.Add( 'ScaleNPCDamage', 'DeathAnimationNPCHitgroup', function( npc, hitgroup, dmginfo ) -- This is only used to set the last hit NPC hitgroup
-	if IsValid( npc ) then
-		LastDamageData[ npc ] = LastDamageData[ npc ] or {}
-		LastDamageData[ npc ].hit = hitgroup
-		LastDamageData[ npc ].backupdinfo = dmginfo -- This dmginfo is only used as a backup
-	end
-end )
-
-hook.Add( 'EntityTakeDamage', 'DeathAnimationDMGInfo', function( target, dmginfo ) -- Used to set the dmginfo for NPCs
-	if IsValid( target ) and target:IsNPC() then
-		LastDamageData[ target ] = LastDamageData[ target ] or {}
-		LastDamageData[ target ].dinfo = dmginfo
-	end
-end )
-
-hook.Add( 'DoPlayerDeath', 'DeathAnimation', function( victim, attacker, dmginfo )
+hook.Add( 'PlayerDeath', 'DeathAnimation', function( victim, inflictor, attacker )
 
 	if !enabled:GetBool() then return end -- Don't do anything if the convar isn't enabled
 	
 	if !IsValid( victim ) then return end -- We need a valid victim for this
 	
+	if onground:GetBool() then -- Don't do anything if they're not allowed to die off ground
+		if !victim:OnGround() then return end
+	end
+	
+	victim.LetRespawn = false -- Don't let them respawn
+	
 	if IsValid( victim:GetRagdollEntity() ) then -- Remove the default ragdoll
 		victim:GetRagdollEntity():Remove()
 	end
 	
-	local seq = CheckAnimation( GetAppropriateAnimation( victim, victim:LastHitGroup(), dmginfo ), victim )
+	local seq = "death_0"..math.random( 1, 4 ) -- Just in case
 	
-	if seq == '%no_anim_norag%' then return end
-
-	local rag = ents.Create( 'prop_ragdoll' )
-	SetEntityStuff( rag, victim )
-	rag:Spawn()
-	rag:SetCollisionGroup( COLLISION_GROUP_WORLD )
-
-	TransferBones( victim, rag ) -- Transfer the bones early just in case they don't get transferred later
-	
-	rag.GetPlayerColor = function() return Vector( victim:GetInfo( "cl_playercolor" ) ) end
-	
-	net.Start( 'DeathAnimationClient' )
-	net.WriteEntity( victim )
-	net.WriteInt( rag:EntIndex(), 32 ) -- The ragdoll doesn't exist on the client yet
-	net.Broadcast()
-	
-	victim.Ragdoll = rag
-	
-	if seq == '%no_anim_ragdoll%' then return end
-	
-	victim.DoNotRespawn = true -- Don't let them respawn
-	AllowBoneMovement( rag, false )
+	local hitgroup = victim:LastHitGroup()
+	if ( hitgroup == HITGROUP_HEAD ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_headshot' ), victim ) -- This goes through getting the right sequence based on the convar
+	elseif ( hitgroup == HITGROUP_CHEST ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_chestshot' ), victim )
+	elseif ( hitgroup == HITGROUP_STOMACH ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_stomachshot' ), victim )
+	elseif ( hitgroup == HITGROUP_LEFTARM ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_leftarm' ), victim )
+	elseif ( hitgroup == HITGROUP_RIGHTARM ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_rightarm' ), victim )
+	elseif ( hitgroup == HITGROUP_LEFTLEG ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_leftleg' ), victim )
+	elseif ( hitgroup == HITGROUP_RIGHTLEG ) then seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_rightleg' ), victim )
+	else seq = CheckForRandomAnim( victim:GetInfo( 'deathanimation_generic' ), victim )
+	end
 	
 	local animent = ents.Create( 'base_gmodentity' ) -- The entity used as a reference for the bone positioning
-	animent:SetModel( GetProperPlayerModel( victim ) )
+	animent:SetModel( victim:GetModel() )
 	animent:SetPos( victim:GetPos() )
 	animent:SetAngles( victim:GetAngles() )
-	animent:SetNoDraw( true ) -- The ragdoll is the thing getting seen
 	animent:Spawn()
-	
-	rag:DeleteOnRemove( animent )
-	
+	animent:Activate()
+	animent:SetNoDraw( true ) -- The ragdoll is the thing getting seen
 	animent:SetSequence( animent:LookupSequence( seq ) ) -- If the sequence isn't valid, the sequence length is 0, so the timer takes care of things
 	animent:SetPlaybackRate( 1 )
 	animent.AutomaticFrameAdvance = true
 	
-	animent:SetSolid( SOLID_OBB ) -- This stuff isn't really needed, but just for physics
+	-- The animent's physics are disabled for now since I'm testing the best way to do it
+	--[[animent:SetSolid( SOLID_OBB ) -- This stuff isn't really needed, but just for physics
 	animent:PhysicsInit( SOLID_OBB )
 	animent:SetMoveType( MOVETYPE_FLYGRAVITY )
 	animent:SetCollisionGroup( COLLISION_GROUP_DEBRIS )
-	animent:PhysWake()
+	local physobj = animent:GetPhysicsObject()
+	if IsValid( physobj ) then
+		physobj:Wake()
+	end]]
+	
+	local rag = ents.Create( 'prop_ragdoll' )
+	SetEntityStuff( rag, victim )
+	rag:Spawn()
+	rag:Activate()
+	AllowBoneMovement( rag, false )
+	
+	rag.GetPlayerColor = victim.GetPlayerColor -- Set the color serverside
+	net.Start( 'DeathAnimation_RagColor' ) -- Set the color clientside
+		net.WriteEntity( rag )
+		net.WriteNormal( victim:GetPlayerColor() )
+	net.Broadcast()
+	
+	victim.Ragdoll = rag
 	
 	function animent:Think() -- This makes the animation work
 		TransferBones( animent, rag )
@@ -210,38 +135,24 @@ hook.Add( 'DoPlayerDeath', 'DeathAnimation', function( victim, attacker, dmginfo
 	end
 	
 	victim:Spectate( OBS_MODE_CHASE ) -- Make them spectate
-	victim:SpectateEntity( rag ) -- Spectate the ragdoll
+	victim:SpectateEntity( rag ) -- Spectate this entity
 	
 	timer.Simple( animent:SequenceDuration( seq ), function() -- After the sequence is done, remove the animation reference
-	
-		if IsValid( animent ) then animent:Remove() end
+		animent:Remove()
 		AllowBoneMovement( rag, true )
-		
-		victim.DoNotRespawn = false -- Let them respawn now
-		
-		if victim.AddCleanup then -- Add the ragdoll to the cleanup list
-			victim:AddCleanup( "ragdolls", rag )
-		end
-		
-		undo.Create( 'ragdoll' ) -- Add the ragdoll to the undo list
-		undo.AddEntity( rag )
-		undo.SetPlayer( victim )
-		undo.Finish()
-		
+		victim.LetRespawn = true -- Let them respawn now
 	end )
 	
 end )
 
 hook.Add( 'PlayerDeathThink', 'DeathAnimationThink', function( ply )
 	if enabled:GetBool() then -- Don't do anything if the convar is not enabled
-		if ply.DoNotRespawn then return false end -- Don't let the player respawn yet
+		if !ply.LetRespawn then return false end -- Don't let the player respawn yet
 	end
 end )
 
 local function CheckAndRemoveRagdoll( ply ) -- Remove the player's ragdoll when they respawn or disconnect
-	if !keeprags:GetBool() then -- Don't do anything if the convar is not enabled
-		if IsValid( ply.Ragdoll ) then ply.Ragdoll:Remove() end
-	end
+	if IsValid( ply.Ragdoll ) then ply.Ragdoll:Remove() end
 end
 
 hook.Add( 'PlayerSpawn', 'DeathAnimationRemoveRagdoll', CheckAndRemoveRagdoll )
